f33640d8d39a63d71fcceb94f91676c5213f93d8
Lã Quốc Nghị <72339471+fakekey@users.noreply.github.com>
2024-05-13 21:06:07 +0700

    Update offset

--- Offsets.cpp
+++ Offsets.cpp
@@ -2,14 +2,14 @@
 
 Offsets::Offsets() {};
 
-int Offsets::GameTime = 0x18f29f8;
-int Offsets::LocalPlayer = 0x19040b0;
-int Offsets::ChampionList = 0x18e3730;
-int Offsets::MinionList = 0x18e8c70;
-int Offsets::MissileList = 0x18E8C80;
-int Offsets::ViewProjMatrix = 0x1957cd0;
+int Offsets::GameTime = 0x190EF28;
+int Offsets::LocalPlayer = 0x19216F0;
+int Offsets::ChampionList = 0x18FFBE0;
+int Offsets::MinionList = 0x19051B0;
+int Offsets::MissileList = 0x19051C0;
+int Offsets::ViewProjMatrix = 0x1974CF0;
 
-int Offsets::ChatClient = 0x1904f40;
+int Offsets::ChatClient = 0x19218C8;
 int Offsets::ChatIsOpen = 0x588;
 
 int Offsets::ObjIndex = 0x10;
@@ -33,17 +33,17 @@ int Offsets::ObjArmor = 0x1954;
 int Offsets::ObjMagicRes = 0x195C;
 int Offsets::ObjMoveSpeed = 0x196C;
 int Offsets::ObjAttackRange = 0x1974;
-int Offsets::ObjName = 0x4400;
-int Offsets::ObjLevel = 0x4E28;
+int Offsets::ObjName = 0x4430;
+int Offsets::ObjLevel = 0x4E58;
 
-int Offsets::ObjSpellBook = 0x3C88;
+int Offsets::ObjSpellBook = 0x3CB0;
 int Offsets::SpellSlotSpellInfo = 0x130;
 int Offsets::SpellInfoSpellData = 0x50;
 int Offsets::SpellDataSpellName = 0x80;
 int Offsets::SpellSlotLevel = 0x28;
 int Offsets::SpellSlotTime = 0x30;
 
-int Offsets::AIManagerAddr = 0x42B8;
+int Offsets::AIManagerAddr = 0x42E8;
 int Offsets::AIManagerStartPath = 0x2F0;
 int Offsets::AIManagerEndPath = 0x2FC;
 int Offsets::AIManagerTargetPos = 0x308;
@@ -56,7 +56,7 @@ int Offsets::AiManagerCurrentSegment = 0x2E0;
 int Offsets::ServerPos = 0x54C;
 int Offsets::Velocity = 0x440;
 
-int Offsets::ObjBuffManager = 0x2F30;
+int Offsets::ObjBuffManager = 0x2F58;
 int Offsets::BuffArrayBegin = 0x18;
 int Offsets::BuffArrayEnd = 0x20;
 int Offsets::BuffEntryBuff = 0x10;
@@ -77,11 +77,11 @@ int Offsets::MissileEndPos = 0x3ac;
 int Offsets::MissileInfo = 0x2E8;
 int Offsets::MissileSrcIdx = 0x370;
 
-int Offsets::ActiveSpellRoot = 0x31C0;
+int Offsets::ActiveSpellRoot = 0x31E8;
 int Offsets::ActiveSpellInfo = 0x8;
 int Offsets::ActiveSpellStartPos = 0xC0;
 int Offsets::ActiveSpellEndPos = 0xCC;
 
-int Offsets::ObjItemList = 0x4ec0;
+int Offsets::ObjItemList = 0x4EF0;
 int Offsets::ItemInfo = 0x30;
 int Offsets::ItemInfoId = 0x9c;
